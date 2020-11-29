d1515cee379e702568c8c0d5dcadddee114da26f
ashdaily <ashishsinghbardhan@gmail.com>
2020-11-29 21:20:51 +0900

    function with arguments

--- 8lesson.js
+++ 8lesson.js
@@ -1,8 +1,18 @@
 // function with arguments or variables or properties
-function sayHello(customerName, ticketNumber){
+function sayHello(customerName, ticketNumber){  //  arguments or parameters
     console.log(`Hello ${customerName}, your food ticket number is ${ticketNumber}`)
 }
 
 sayHello("Ash", 1)
 sayHello("Miki", 2)
 sayHello("Shinichi", 3)
+
+/*
+function myFunctionName(arg1, arg2, arg3){
+    // you  can use these arguments here
+
+
+}
+
+myFunctionName({}, [], "")
+*/
