ae4ed6c440be9147910cd7a84695ecdcf351a147
ashdaily <ashishsinghbardhan@gmail.com>
2020-12-03 22:14:55 +0900

    23lesson.js

---
+++ 23lesson.js
@@ -0,0 +1,3 @@
+const string =  "abc locate abc locate abc"
+console.log(string.indexOf("locate")) // will check first locate and return first alphabet's index
+console.log(string.lastIndexOf("locate")) // will check last locate and return first alphabet's index
